87ea106bc3a743b34bf73846c3de952041a323bd
ahmetatalay <ahmet@opsgenie.com>
2016-08-05 09:46:08 +0300

    sout

--- src/main/java/AdapterPattern/AdapterPatternDemo.java
+++ src/main/java/AdapterPattern/AdapterPatternDemo.java
@@ -8,7 +8,7 @@ import AdapterPattern.classicalMedia.AudioPlayer;
 public class AdapterPatternDemo {
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AudioPlayer();
-
+        System.out.println("test");
         audioPlayer.play("mp3", "beyond the horizon.mp3");
         audioPlayer.play("mp4", "alone.mp4");
         audioPlayer.play("vlc", "far far away.vlc");
